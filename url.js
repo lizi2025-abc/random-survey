1c30c90a769510c4dc171adccaed5163d1d6ac5f
lizi2025-abc <411074028@gms.ndhu.edu.tw>
2025-05-21 12:37:16 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/asCYU1MwhiSqKfdt5',
+	'https://forms.gle/bmySKq9RsyySXdR36'
 ];
